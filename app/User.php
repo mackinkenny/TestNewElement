7a08d1c65f7c4837606425be9c85e86ae57b16aa
Erkin <Mackinkenny@gmail.com>
2022-03-03 23:28:34 +0600

    Encryption of personal data

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use ESolution\DBEncryption\Traits\EncryptedAttribute;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,6 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    use EncryptedAttribute;
 
     /**
      * The attributes that are mass assignable.
@@ -35,6 +37,15 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The attributes that should be encrypted on save.
+     *
+     * @var array
+     */
+    protected $encryptable = [
+        'name', 'email', 'phone'
     ];
 
 
